9eb91fb77a82d2512add4632d8baf80a1dc916e0
hekun <1033699689@qq.com>
2020-07-29 16:45:28 +0800

    直接推送到github

---
+++ git_github/src/main/java/club/znyf/test1.java
@@ -0,0 +1,11 @@
+package club.znyf;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 16:40
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("asdfsafsdffs");
+    }
+}
