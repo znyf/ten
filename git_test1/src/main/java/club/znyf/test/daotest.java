35c8976676dcaaf713654fe9a48e9ec93328811a
hekun <1033699689@qq.com>
2020-07-29 10:42:31 +0800

    添加了一个test

---
+++ git_test1/src/main/java/club/znyf/test/daotest.java
@@ -0,0 +1,15 @@
+package club.znyf.test;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 10:19
+ */
+public class daotest {
+    public static void main(String[] args) {
+        System.out.println(12312123);
+    }
+
+    public void test(){
+        System.out.println(1231232);
+    }
+}
