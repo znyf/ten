fcbecfd6163f8ca9f373460e89ae468bed6b5a9b
hekun <1033699689@qq.com>
2020-07-29 10:48:02 +0800

    添加test到git

---
+++ git_test/src/main/java/club/znyf/test.java
@@ -0,0 +1,22 @@
+package club.znyf;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 9:55
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("asdfasdfafsafdafasf");
+        System.out.println("asdfasdfafsafdafasf");
+        System.out.println("asdfasdfafsafdafasf");
+    }
+
+    public void test(){
+        System.out.println("123213");
+        System.out.println("123213");
+        System.out.println("123213");
+        System.out.println("123213");
+        System.out.println("123213");
+        System.out.println("123213");
+    }
+}
