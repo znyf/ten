652ffa6ad859fdc85449b18a1562f1e671db7979
hekun <1033699689@qq.com>
2020-07-29 16:33:50 +0800

    上传一个项目到github

---
+++ git_test_github/src/main/java/club/znyf/test1.java
@@ -0,0 +1,11 @@
+package club.znyf;
+
+/**
+ * @author hk
+ * @data 2020/7/29 - 11:22
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("第一次将idea项目推送到github");
+    }
+}
